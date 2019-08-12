c118eccc1880eedfeca93fe8687a3beed4afb861
a1349954422 <51180937+a1349954422@users.noreply.github.com>
2019-08-12 16:44:36 +0800

    Add files via upload
    在贺卡中添加一个图片

---
+++ chapter5/XmasCard1/code/Consts.java
@@ -0,0 +1,17 @@
+package XmasCard.code;
+
+public class Consts {//定义一些常量
+
+    public Consts() {
+    }
+
+    public static final int minx_dis = 50;
+    public static final int maxx_dis = 450;
+    public static final int miny_dis = 50;
+    public static final int maxy_dis = 350;
+    public static final int maxy = 400;
+    public static final int maxx = 500;
+    public static final int MARGIN = 50;
+
+
+}
