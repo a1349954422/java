593d70d458445416fcc22bdfa0c8450abb5e4be5
a1349954422 <1349954422@qq.com>
2019-09-05 19:04:47 +0800

    问题程序

---
+++ chapter7/CountGames/GameCanvas.java
@@ -0,0 +1,33 @@
+package chapter7.CountGames;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameCanvas extends JPanel {
+    private Playerp playerp;
+    private Board board;
+
+    public GameCanvas() {
+        setPreferredSize(new Dimension(Consts.CV_WIDTH,Consts.PLAYER_PANEL_H + Consts.BOARD_H));
+        this.setLayout(new BorderLayout());
+        initCanvas();
+    }
+
+    private void initCanvas() {
+        initBoards();
+        initPlayerP();
+    }
+
+    private void initBoards() {
+        board = new Board();
+        board.setPlayerp(playerp);
+        add(board,BorderLayout.SOUTH);
+    }
+
+    private void initPlayerP() {
+        playerp = new Playerp();
+        add(playerp,BorderLayout.NORTH);
+        playerp.setBoard(board);
+
+    }
+}
---
+++ chapter7/CountGames/Playerp.java
@@ -0,0 +1,159 @@
+package chapter7.CountGames;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Playerp extends JPanel {
+    private JComboBox numComboBox;
+    private String[] numArray;
+    private JLabel numLbl;
+
+    private JComboBox diffComboBox;
+    private String[] diffArray;
+    private JLabel diffLbl;
+
+    private JTextField countDisplay;
+    private JLabel countMessage;
+    private JTextField inputAnswer;
+    private JLabel answerMessage;
+
+    private JLabel scoreLbl;
+    private JTextField scoreTxt;
+
+    private JButton morePlayIngBtn;
+    private JButton terminalBtn;
+
+    private JPanel numSelP;
+    private JPanel diffP;
+    private JPanel selP;
+    private JPanel disPlayP;
+    private JPanel scoreP;
+
+    private Board board;
+    private Playerp playerp;
+
+    public Playerp() {
+        this.setPreferredSize(new Dimension(Consts.CV_WIDTH,Consts.PLAYER_PANEL_H));
+        this.setLayout(new BorderLayout());
+        initPlayPenal();
+
+    }
+
+    private void initPlayPenal() {
+        ActionListener myActionListener = new MyActionListner();
+        numSelP = new JPanel();
+        numSelP.setPreferredSize(new Dimension(numSelP.getPreferredSize().width,85));
+        EmptyBorder eb = new EmptyBorder(new Insets(30,30,5,30));
+        numSelP.setBorder(eb);
+        numSelP.setLayout(new FlowLayout());
+        numArray = new String[]{"1","2","3","4","5","6","7","8"};
+        numComboBox = new JComboBox(numArray);
+        numLbl = new JLabel("please select how much count do you want");
+        Dimension d = numComboBox.getPreferredSize();
+        numComboBox.setPreferredSize(new Dimension(120,d.height));
+        numComboBox.addActionListener(myActionListener);
+        numSelP.add(numLbl);
+        numSelP.add(numComboBox);
+
+        diffP = new JPanel();
+        diffP.setPreferredSize(new Dimension(diffP.getPreferredSize().width,85));
+        EmptyBorder eb2 = new EmptyBorder(new Insets(30,30,5,30));
+        diffP.setBorder(eb2);
+        diffP.setLayout(new FlowLayout());
+        diffArray = new String[]{"easy","middle","hard"};
+        diffComboBox = new JComboBox(diffArray);
+        diffLbl = new JLabel("please select a diffculties");
+        Dimension d1 = diffComboBox.getPreferredSize();
+        diffComboBox.setPreferredSize(new Dimension(120,d1.height));
+        diffComboBox.addActionListener(myActionListener);
+        diffP.add(diffLbl);
+        diffP.add(diffComboBox);
+
+        selP = new JPanel();
+        selP.setLayout(new BorderLayout());
+        selP.add(numSelP,BorderLayout.NORTH);
+        selP.add(diffP,BorderLayout.SOUTH);
+
+        disPlayP = new JPanel();
+        EmptyBorder eb3 = new EmptyBorder(new Insets(5,30,20,30));
+        disPlayP.setBorder(eb3);
+        countDisplay = new JTextField(20);
+        countMessage = new JLabel("count:");
+        inputAnswer = new JTextField(20);
+        inputAnswer.addActionListener(myActionListener);
+        answerMessage = new JLabel("answer");
+        morePlayIngBtn = new JButton("more playing");
+        morePlayIngBtn.addActionListener(myActionListener);
+        terminalBtn = new JButton("terminal");
+        terminalBtn.addActionListener(myActionListener);
+        disPlayP.add(countMessage);
+        disPlayP.add(countDisplay);
+        disPlayP.add(answerMessage);
+        disPlayP.add(inputAnswer);
+        disPlayP.add(morePlayIngBtn);
+        disPlayP.add(terminalBtn);
+
+        scoreP = new JPanel();
+        scoreLbl = new JLabel("total score:");
+        scoreTxt = new JTextField(20);
+        scoreP.add(scoreLbl);
+        scoreP.add(scoreTxt);
+
+        this.add(selP,BorderLayout.NORTH);
+        this.add(disPlayP,BorderLayout.CENTER);
+        this.add(scoreP,BorderLayout.SOUTH);
+    }
+
+     class MyActionListner implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == numComboBox){
+                diffComboBox.setEnabled(true);
+                numComboBox.setEnabled(false);
+                inputAnswer.setEnabled(false);
+                board.setNums((String)numComboBox.getSelectedItem());
+            }else if(e.getSource() == diffComboBox){
+                numComboBox.setEnabled(false);
+                diffComboBox.setEnabled(false);
+                inputAnswer.setEnabled(true);
+                board.setDiff((String)diffComboBox.getSelectedItem());
+                board.initGame();
+                inputAnswer.setText("");
+                inputAnswer.requestFocus();
+            }else if(e.getSource() == inputAnswer){
+                board.receiveAnswer(inputAnswer.getText());
+                numComboBox.setEnabled(false);
+                diffComboBox.setEnabled(false);
+                inputAnswer.setEnabled(false);
+                inputAnswer.setText("");
+                inputAnswer.requestFocus();
+            }
+            else if(e.getSource() == morePlayIngBtn){
+                diffComboBox.setEnabled(false);
+                numComboBox.setEnabled(true);
+                inputAnswer.setEnabled(false);
+                board.setInit(true);
+                board.repaint();
+            }else if(e.getSource() == terminalBtn){
+                System.exit(0);
+            }
+
+        }
+    }
+
+    public void setCount(String s){
+        countDisplay.setText(s);
+    }
+
+    public void setScore(String n){
+        scoreTxt.setText(n);
+    }
+
+    public void setBoard(Board board){
+        this.board = board;
+    }
+
+}
